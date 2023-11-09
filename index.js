53af4a03af06669bbcd3b423ea6e5e4313b654a8
Willian Henkel <willian.deus@hitechnologies.com.br>
2023-11-08 21:50:12 -0300

    added certificate service

--- index.js
+++ index.js
@@ -1,87 +1,44 @@
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
-const pdf = require('html-pdf');
-const passengers = require('./mock/passengers');
-const puppeteer = require('puppeteer');
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
+const puppeteer = require('puppeteer')
+const app = express()
+const passengers = require('./mocks/passengers.js');
 
-const app = express();
+app.get('/pdf', async(request, response) => {
 
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-app.get('/pdf', async (req, res) => {
+    await page.goto('http://localhost:3000/', {
+        waitUntil: 'networkidle0'
+    })
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    const pdf = await page.pdf({
+        printBackground: true,
+        format: 'Letter'
+    })
 
-  const filePath = path.resolve(__dirname, "template", "index.ejs")
+    await browser.close()
 
-  const html = ejs.renderFile(filePath, { passengers }, (err, html) => {
-    if(err) {
-      return "<h1>error</h1>"
-    }
+    response.contentType("application/pdf")
 
-    // enviar para o navegador
-    return html;
-})
-
-  await page.setContent(html);
-  const pdf = await page.pdf({
-    landscape: true,
-    printBackground: true,
-    format: 'A4'
-  })
-
-  await browser.close();
-
-  res.contentType("application/pdf");
-
-  return res.send(pdf);
+    return response.send(pdf)
 
 })
 
-app.get('/', (req, res) => {
-  const filePath = path.resolve(__dirname, "template", "index.ejs")
-  ejs.renderFile(filePath, { passengers }, (err, html) => {
-      if(err) {
-        return res.json({"error": "Error on reading file"});
-      }
-  
-      // enviar para o navegador
-      return res.send(html)
-  })
+app.get('/', (request, response) => {
+
+    const filePath = path.join(__dirname, "template", "index.ejs")
+    ejs.renderFile(filePath, { passengers }, (err, html) => {
+        if(err) {
+            return response.send('Erro na leitura do arquivo')
+        }
+    
+        // enviar para o navegador
+        return response.send(html)
+    })
+   
 })
 
-// app.get('/', (req, res) => {
-  
-//   const file = path.resolve(__dirname, "template", "index.ejs");
-
-//   ejs.renderFile(file, { passengers }, (err, html) => {
-//     if(err) {
-//       return res.json({"error": "Error on reading file"});
-//     }
-
-//     const options = {
-//       height: "11.25in",
-//       width: "8.5in",
-//       header: {
-//           height: "20mm"
-//       },
-//       footer: {
-//           height: "20mm"
-//       }
-//     }
-
-//     pdf.create(html, options).toFile("certificado.pdf", (err, data) => {
-//       if(err) {
-//         return res.json({"error": "error on generating pdf"});
-//       }
-
-//       return res.send(html)
-
-//     })
-
-//   })
-
-// })
-
-app.listen(3000, () => console.log('Certificate service is running at http://localhost:3000'));
+app.listen(3000)
