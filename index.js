ca51f9de6e991e3e3e7a16a73ec560bb31a3f461
Willian Henkel <willian.deus@hitechnologies.com.br>
2023-11-09 17:19:17 -0300

    added second page

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const passengers = require('./mocks/passengers.js');
 
 app.get('/pdf', async(request, response) => {
 
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
 
-    await page.goto('http://localhost:3000/', {
+    await page.goto('http://localhost:8080/', {
         waitUntil: 'networkidle0'
     })
 
     const pdf = await page.pdf({
+        path: path.join(__dirname, 'certificates', 'certificate.pdf'),
         printBackground: true,
-        format: 'Letter'
+        format: 'A4',
+        landscape: true
     })
 
     await browser.close()
+
 
     response.contentType("application/pdf")
 
@@ -29,7 +32,9 @@ app.get('/pdf', async(request, response) => {
 
 app.get('/', (request, response) => {
 
-    const filePath = path.join(__dirname, "template", "index.ejs")
+    const filePath = path.join(__dirname, "template", "certificate.ejs")
+
+    
     ejs.renderFile(filePath, { passengers }, (err, html) => {
         if(err) {
             return response.send('Erro na leitura do arquivo')
@@ -41,4 +46,4 @@ app.get('/', (request, response) => {
    
 })
 
-app.listen(3000)
+app.listen(8080, () => console.log("Server is running at port http://localhost:8080"))
